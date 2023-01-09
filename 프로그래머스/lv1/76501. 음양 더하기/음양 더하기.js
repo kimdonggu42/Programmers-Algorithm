0d2fcfa79e6fd4d54e305a5330fa896f98e536d6
kimdonggu42 <115632555+kimdonggu42@users.noreply.github.com>
2023-01-09 14:23:38 +0900

    [level 1] Title: 음양 더하기, Time: 0.10 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv1/76501. 음양 더하기/음양 더하기.js
+++ 프로그래머스/lv1/76501. 음양 더하기/음양 더하기.js
@@ -1,40 +1,35 @@
-// 1. signs[index]가 false면 value에 -를 붙여서 리턴하고,
-// signs[index]가 true면 value를 그대로 리턴한다.
-// 2. 리턴된 새로운 배열들의 합을 구한다.
+// 1. i번째 signs가 false면 같은 i번째 absolutes값으 빼주고,
+// 2. i번째 signs가 true면 같은 i번째 absolutes값으 더해준다.
 
 function solution(absolutes, signs) {
-  let num = absolutes.map((value, index) => {
-    if (signs[index] === false) {
-      return -value;
-    } else {
-      return value;
-    }
-  });
+  let sum = 0;
 
-  let sum = num.reduce((acc, cur) => {
-    return acc + cur;
-  });
+  for (let i = 0; i < absolutes.length; i++) {
+    if (signs[i] === false) {
+      sum = sum - absolutes[i];
+    } else {
+      sum = sum + absolutes[i];
+    }
+  };
   return sum;
 };
 
 
-// 1. 빈 배열에 signs[i]가 false이면 같은 absolutes[i]에 -를 붙여서 담아주고, true면 absolutes[i]를 그대로 담아준다.
-// 2. 빈 배열에 담긴 모든 요소의 합을 구한다.
+// 다른 방법
+// 1. signs[index]가 false면 value에 -를 붙여서 리턴하고,
+// signs[index]가 true면 value를 그대로 리턴한다.
+// 2. 리턴된 새로운 배열들의 합을 구한다.
 
 // function solution(absolutes, signs) {
-//     let result = [];
-//     let sum = 0;
-    
-//     for (let i = 0; i <absolutes.length; i++) {
-//         if (signs[i] === false) {
-//             result.push(-absolutes[i]);
-//         } else {
-//             result.push(absolutes[i]);
-//         }
+//   let num = absolutes.map((value, index) => {
+//     if (signs[index] === false) {
+//       return -value;
+//     } else {
+//       return value;
 //     }
-    
-//     for (let j = 0; j < result.length; j++) {
-//         sum = sum + result[j];
-//     }
-//     return sum;
-// }
+//   });
+
+//   return num.reduce((acc, cur) => {
+//     return acc + cur;
+//   });
+// };
