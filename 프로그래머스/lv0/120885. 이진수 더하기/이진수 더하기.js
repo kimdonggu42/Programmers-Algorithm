ee8134cf5336fd4e34b0d0173703e96fd8b76a1f
kimdonggu42 <115632555+kimdonggu42@users.noreply.github.com>
2023-01-12 00:34:53 +0900

    [level 0] Title: 이진수 더하기, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120885. 이진수 더하기/이진수 더하기.js
+++ 프로그래머스/lv0/120885. 이진수 더하기/이진수 더하기.js
@@ -6,7 +6,5 @@
 // 2. 더한 결과값을 다시 2진법으로 변환(toString)
 
 function solution(bin1, bin2) {
-    let binSum = parseInt(Number(bin1), 2) + parseInt(Number(bin2), 2)
-    return binSum.toString(2)
-    
+    return (parseInt(Number(bin1), 2) + parseInt(Number(bin2), 2)).toString(2);
 }
