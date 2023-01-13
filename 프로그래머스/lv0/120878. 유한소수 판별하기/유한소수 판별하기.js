c4e6cdae752c48843f5fbe9faedd11bd1384e304
kimdonggu42 <115632555+kimdonggu42@users.noreply.github.com>
2023-01-14 02:22:50 +0900

    [level 0] Title: 유한소수 판별하기, Time: 0.16 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120878. 유한소수 판별하기/유한소수 판별하기.js
+++ 프로그래머스/lv0/120878. 유한소수 판별하기/유한소수 판별하기.js
@@ -1,50 +1,58 @@
-// 소인수 : 약수 중 소수(약수가 1과 자기자신만 있는 수)인 수
-// a/b가 유한소수이면 1 리턴
-// a/b가 무한소수이면 2 리턴
+// 소인수 : 약수 중 소수인 수 === 약수 중 1과 자기 자신으로만 나누어 떨어지는 수
+// 유한소수 : 기약분수 분모의 약수들이 모두 2 또는 5로 나누어 떨어지는 경우 === 기약분수 분모의 소인수가 2 또는 5만 존재하는 경우
 
-function solution(a, b) {  // a: 7, b: 20 
-    let GCD = 0;  // 최대공약수
+function solution(a, b) {
     // 최대공약수 구하기
+    let GCD = 0;
+
     for (let i = 1; i <= Math.min(a, b); i++) {
         if (a % i === 0 && b % i === 0) {
             GCD = i;  // 1
-        }
-    }
-    // 기약분수 분모의 약수 구하기
-    let denominator = b / GCD  // 기약분수 분모
-    let factorsOfB = [];  // 기약분수 분모의 약수
+        };
+    };
+
+    // (기약분수)분모의 약수 구하기
+    let denominator = b / GCD
+    let factorsOfB = [];
 
     for (let j = 2; j <= denominator; j++) {
         if (denominator % j === 0) {
-            factorsOfB.push(j);  // [2, 4, 5, 10, 20]
-        }
+            factorsOfB.push(j);  // [2, 4, 5, 10, 20], [3, 7, 17, 21, 51, 119, 357]
+        };
+    };
+
+    // 유한소수 판별하기
+    // factorsOfB에서 2와 5 둘 다 나누어 떨어지지 않는 요소들을 새로운 배열로 반환한다.
+    let result = factorsOfB.filter((value) => {
+        return value % 2 !== 0 && value % 5 !== 0
+    });
+
+    if (result.length === 0) {
+        return 1;
+    } else {
+        return 2;
     }
-    return factorsOfB.find(n => n % 2 !== 0 && n % 5 !== 0) ? 2 : 1;
 }
 
 
-// function solution(a, b) {  // a: 7, b: 20 
-//     let GCD = 0;  // 최대공약수
+// function solution(a, b) {
+//     let GCD = 0;
 
-//     // 최대공약수 구하기
 //     for (let i = 1; i <= Math.min(a, b); i++) {
 //         if (a % i === 0 && b % i === 0) {
 //             GCD = i;  // 1
 //         }
 //     }
 
-//     // 기약분수 분모의 약수 구하기
-//     let denominator = b / GCD  // 기약분수 분모
-//     let factorsOfB = [];  // 기약분수 분모의 약수
+//     let denominator = b / GCD
+//     let factorsOfB = [];
 
 //     for (let j = 2; j <= denominator; j++) {
 //         if (denominator % j === 0) {
-//             factorsOfB.push(j);  // [2, 4, 5, 10, 20]
+//             factorsOfB.push(j);  // [2, 4, 5, 10, 20], [3, 7, 17, 21, 51, 119, 357]
 //         }
 //     }
 
-//     // 기약분수 분모의 약수 중 소수(분모의 소인수) 구하기 -> 1은 소수 아님
-//     // result에 2 또는 5만 존재하거나, 2와 5가 존재해야한다.
 //     let result = [];
 //     let prime = true;
 
