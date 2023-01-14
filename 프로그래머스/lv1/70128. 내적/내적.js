dfe9181dd648f5e4aab3fa514a116f1f6eaea948
kimdonggu42 <115632555+kimdonggu42@users.noreply.github.com>
2023-01-15 00:24:39 +0900

    [level 1] Title: 내적, Time: 0.08 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv1/70128. 내적/내적.js
+++ 프로그래머스/lv1/70128. 내적/내적.js
@@ -1,6 +1,7 @@
 function solution(a, b) {
     let result = 0;
 
+    // a와 b의 같은 index의 요소끼리 곱해서 result에 더해준다.
     for (let i = 0; i < a.length; i++) {
         result = result + a[i] * b[i];
     }
