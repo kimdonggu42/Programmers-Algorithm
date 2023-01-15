50f403a221a778c74d3ad9565b3fbe1011086d07
kimdonggu42 <115632555+kimdonggu42@users.noreply.github.com>
2023-01-15 23:47:30 +0900

    [level 0] Title: 치킨 쿠폰, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120884. 치킨 쿠폰/치킨 쿠폰.js
@@ -0,0 +1,14 @@
+// 1. 1081장 / 10 = 몫 : 서비스 치킨 108마리, 나머지 : 쿠폰 1장
+// 2. 108장 / 10 = 몫 : 서비스 치킨 10마리, 나머지 : 쿠폰 8장
+// 3. 10장 / 10 = 몫 : 서비스 치킨 1마리, 나머지 : 쿠폰 1장
+// 4. 나머지가 누적 10이면 서비스 치킨 1마리 추가
+// 108 + 10 + 1 + 1 = 120
+
+function solution(chicken) {
+    let serviceChicken = 0;
+    
+    for (let i = chicken; 1 <= i; i = i / 10) {
+        serviceChicken = serviceChicken + i
+    }
+    return Math.floor(serviceChicken / 10)
+}
