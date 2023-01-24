f430dfa8c02c43bbaa3d3aceaf32516d9256e20b
kimdonggu42 <115632555+kimdonggu42@users.noreply.github.com>
2023-01-24 18:08:47 +0900

    [level 1] Title: x만큼 간격이 있는 n개의 숫자, Time: 0.27 ms, Memory: 33.8 MB -BaekjoonHub

--- 프로그래머스/lv1/12954. x만큼 간격이 있는 n개의 숫자/x만큼 간격이 있는 n개의 숫자.js
+++ 프로그래머스/lv1/12954. x만큼 간격이 있는 n개의 숫자/x만큼 간격이 있는 n개의 숫자.js
@@ -16,3 +16,13 @@ function solution(x, n) {
     }
     return result;
 }
+
+
+// function solution(x, n) {
+//     let result = [];
+
+//     for (let i = 1; i <= n; i++) {
+//         result.push(x * i);
+//     }
+//     return result;
+// }
