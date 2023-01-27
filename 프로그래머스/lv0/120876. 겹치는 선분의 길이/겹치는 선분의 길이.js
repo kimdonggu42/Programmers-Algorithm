98d586122f16dfbcdc250560b0ab6e5a8b2b1778
kimdonggu42 <115632555+kimdonggu42@users.noreply.github.com>
2023-01-27 16:41:05 +0900

    [level 0] Title: 겹치는 선분의 길이, Time: 0.13 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120876. 겹치는 선분의 길이/겹치는 선분의 길이.js
+++ 프로그래머스/lv0/120876. 겹치는 선분의 길이/겹치는 선분의 길이.js
@@ -1,15 +1,11 @@
 // lines === [[0, 1], [2, 5], [3, 9]]
 function solution(lines) {
     let result = 0;
-    let start = 0;
-    let end = 0;
     let linesRange = new Array(200).fill(0);
 
     // 각 선분들이 지나간 자리(index)를 1로 채운다.
     for (let i = 0; i < lines.length; i++) {
-        start = lines[i][0];
-        end = lines[i][1];
-        for (let j = start; j < end; j++) {  // [1, 0, 1, 2, 2, 2]
+        for (let j = lines[i][0]; j < lines[i][1]; j++) {  // [1, 0, 1, 2, 2, 2]
             linesRange[j + 100] = linesRange[j + 100] + 1;
         }
     }
