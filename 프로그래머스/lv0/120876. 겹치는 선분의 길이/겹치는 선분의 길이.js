91414fe53b3e95e56566c271e01ddab8680c6e5b
kimdonggu42 <115632555+kimdonggu42@users.noreply.github.com>
2023-01-27 17:43:04 +0900

    [level 0] Title: 겹치는 선분의 길이, Time: 0.23 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120876. 겹치는 선분의 길이/겹치는 선분의 길이.js
+++ 프로그래머스/lv0/120876. 겹치는 선분의 길이/겹치는 선분의 길이.js
@@ -1,20 +1,31 @@
-// lines === [[0, 1], [2, 5], [3, 9]]
+// lines === [[0, 2], [-3, -1], [-2, 1]]
+// i[0] === [0, 0, 0, 1, 1, 0]
+// i[1] === [1, 1, 0, 1, 1, 0]
+// i[2] === [1, 2, 1, 2, 1, 0]
 function solution(lines) {
-    let result = 0;
-    let linesRange = new Array(200).fill(0);
+  let result = 0;
+  let flatLines = lines.flat();
+  let absFlatLines = [];
 
-    // 각 선분들이 지나간 자리(index)를 1로 채운다.
-    for (let i = 0; i < lines.length; i++) {
-        for (let j = lines[i][0]; j < lines[i][1]; j++) {  // [1, 0, 1, 2, 2, 2]
-            linesRange[j + 100] = linesRange[j + 100] + 1;
-        }
-    }
+  for (let i = 0; i < flatLines.length; i++) {
+    absFlatLines.push(Math.abs(flatLines[i]));
+  }
 
-    // 선분들이 한 번 이상 지나간 구간의 수를 구한다.
-    for (let i = 0; i < linesRange.length; i++) {
-        if (1 < linesRange[i]) {
-            result = result + 1;
-        }
+  let maxNum = Math.max(...absFlatLines);
+  let linesRange = new Array(maxNum * 2).fill(0);
+
+  // 각 선분들이 지나간 자리(index)를 1로 채운다.
+  for (let i = 0; i < lines.length; i++) {
+    for (let j = lines[i][0]; j < lines[i][1]; j++) {
+      linesRange[j + linesRange.length / 2] += 1;
     }
-    return result;
+  }
+
+  // 선분들이 한 번 이상 지나간 구간(2 이상)의 수를 구한다.
+  for (let i = 0; i < linesRange.length; i++) {
+    if (1 < linesRange[i]) {
+      result = result + 1;
+    }
+  }
+  return result;
 }
