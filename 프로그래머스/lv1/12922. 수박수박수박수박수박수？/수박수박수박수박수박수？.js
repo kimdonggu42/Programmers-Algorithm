0c82be888ad250ee53c29183ceaef73891fcc9aa
kimdonggu42 <115632555+kimdonggu42@users.noreply.github.com>
2023-01-27 23:57:26 +0900

    [level 1] Title: 수박수박수박수박수박수?, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.js
+++ 프로그래머스/lv1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.js
@@ -1,13 +1,13 @@
 function solution(n) {
-    let result = ''
-    let str = '수박'
+    let result = '';
+    let str = '수박';
 
     for (let i = 1; i <= n; i++) {
         if (i % 2 !== 0) {
-            result += str[0]
+            result += str[0];
         } else {
-            result += str[1]
+            result += str[1];
         }
     }
-    return result
+    return result;
 }
