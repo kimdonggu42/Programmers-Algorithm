546dda6b1d8e5e2cbf07dad30609c997bf0f4f1f
kimdonggu42 <115632555+kimdonggu42@users.noreply.github.com>
2023-01-28 20:59:49 +0900

    [level 0] Title: 평행, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120875. 평행/평행.js
+++ 프로그래머스/lv0/120875. 평행/평행.js
@@ -1,3 +1,6 @@
+// 전제 조건: 2개의 직선의 기울기가 서로 같으면 평행이다
+// 기울기 공식: (y2 - y1) / (x2 - x1)
+
 // case 1. dots[0]dots[1]의 기울기 === dots[2]dots[3]의 기울기
 // dots[0]dots[1]의 기울기([1, 4], [9, 2]): (dots[1][1] - dots[0][1]) / (dots[1][0] - dots[0][0])
 // dots[2]dots[3]의 기울기([3, 8], [11, 6]): (dots[3][1] - dots[2][1]) / (dots[3][0] - dots[2][0])
@@ -13,16 +16,16 @@
 function solution(dots) {
     let slopeZeroOne = (dots[1][1] - dots[0][1]) / (dots[1][0] - dots[0][0]);
     let slopeTwoThree = (dots[3][1] - dots[2][1]) / (dots[3][0] - dots[2][0]);
-    
+
     let slopeZeroTwo = (dots[2][1] - dots[0][1]) / (dots[2][0] - dots[0][0]);
     let slopeOneThree = (dots[3][1] - dots[1][1]) / (dots[3][0] - dots[1][0]);
-    
+
     let slopeZeroThree = (dots[3][1] - dots[0][1]) / (dots[3][0] - dots[0][0]);
     let slopeOneTwo = (dots[2][1] - dots[1][1]) / (dots[2][0] - dots[1][0]);
-    
+
     if (slopeZeroOne === slopeTwoThree || slopeZeroTwo === slopeOneThree || slopeZeroThree === slopeOneTwo) {
         return 1;
-    } else if (slopeZeroOne !== slopeTwoThree || slopeZeroTwo !== slopeOneThree || slopeZeroThree !== slopeOneTwo) {
+    } else {
         return 0;
     }
-} 
+}
