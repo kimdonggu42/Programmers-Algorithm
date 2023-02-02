432d67184a335b3cd48d7fe8a326fdc192b0013f
kimdonggu42 <115632555+kimdonggu42@users.noreply.github.com>
2023-02-02 18:04:41 +0900

    [level 0] Title: 배열 자르기, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120833. 배열 자르기/배열 자르기.js
+++ 프로그래머스/lv0/120833. 배열 자르기/배열 자르기.js
@@ -1,3 +1,7 @@
+function solution(numbers, num1, num2) {
+    return numbers.slice(num1, num2 + 1);
+}
+
 // function solution(numbers, num1, num2) {
 //     let result = [];
 
@@ -6,7 +10,3 @@
 //     }
 //     return result;
 // }
-
-function solution(numbers, num1, num2) {
-    return numbers.slice(num1, num2 + 1);
-}
