973b3a5e002f3e3f7402479c1fc7d5fad38341db
kimdonggu42 <115632555+kimdonggu42@users.noreply.github.com>
2023-02-18 19:47:11 +0900

    [level 1] Title: 부족한 금액 계산하기, Time: 0.18 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/82612. 부족한 금액 계산하기/부족한 금액 계산하기.js
@@ -0,0 +1,21 @@
+function solution(price, money, count) {
+  let arr = [];
+  let sum = 0;
+
+  for (let i = 1; i <= count; i++) {
+    arr.push(price * i);
+  }
+
+  for (let i = 0; i < arr.length; i++) {
+    sum = sum + arr[i];
+  }
+
+  // 놀이기구를 count번 타는데 필요한 금액의 합(sum)이
+  // 원래 가지고 있던 돈인 money보다 더 많이 든다면 모자란 금액을 리턴하고,  
+  if (sum > money) {
+    return sum - money;
+  // 돈이 모자라지 않는다면 0을 리턴한다.    
+  } else {
+    return 0;
+  }
+}
