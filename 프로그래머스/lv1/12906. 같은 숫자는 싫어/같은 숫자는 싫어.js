cc58ffc15640a51e8782e9c077dff1f11faf0c6b
kimdonggu42 <115632555+kimdonggu42@users.noreply.github.com>
2023-02-26 17:02:02 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 13.61 ms, Memory: 83.2 MB -BaekjoonHub

--- 프로그래머스/lv1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.js
+++ 프로그래머스/lv1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.js
@@ -1,10 +1,22 @@
-function solution(arr) {
-    let result = [];
+// function solution(arr) {
+//     let result = [];
 
-    for (let i = 0; i < arr.length; i++) {
-        if (arr[i] !== arr[i + 1]) {
-            result.push(arr[i]);
+//     for (let i = 0; i < arr.length; i++) {
+//         if (arr[i] !== arr[i + 1]) {
+//             result.push(arr[i]);
+//         }
+//     }
+//     return result;
+// }
+
+function solution(arr) {
+    var answer = [arr[0]];
+
+    for (let i=1; i<arr.length; i++) {
+        if (answer[answer.length - 1] !== arr[i]) {
+            answer.push(arr[i]);
         }
     }
-    return result;
+
+    return answer;
 }
