ec6ffa13dffea5f6a3cc690e17f8ce0f9c7043de
kimdonggu42 <115632555+kimdonggu42@users.noreply.github.com>
2023-02-27 22:22:55 +0900

    [level 1] Title: 이상한 문자 만들기, Time: 0.32 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12930. 이상한 문자 만들기/이상한 문자 만들기.js
@@ -0,0 +1,22 @@
+function solution(s) {
+    let splitWord = s.split(' ').slice();
+    let changeWord = [];
+    let result = [];
+
+    for (let i = 0; i < splitWord.length; i++) {
+        let str = [];
+        for (let j = 0; j < splitWord[i].length; j++) {
+            if (j % 2 === 0) {
+                str.push(splitWord[i][j].toUpperCase());
+            } else {
+                str.push(splitWord[i][j].toLowerCase());
+            }
+        }
+        changeWord.push(str);
+    }
+
+    for (let i = 0; i < changeWord.length; i++) {
+        result.push(changeWord[i].join(''))
+    }
+    return result.join(' ');
+}
