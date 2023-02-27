570092f8021a3318625242a296a3b9bc8b83b171
kimdonggu42 <115632555+kimdonggu42@users.noreply.github.com>
2023-02-27 22:41:43 +0900

    [level 1] Title: 이상한 문자 만들기, Time: 0.19 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv1/12930. 이상한 문자 만들기/이상한 문자 만들기.js
+++ 프로그래머스/lv1/12930. 이상한 문자 만들기/이상한 문자 만들기.js
@@ -1,7 +1,12 @@
-function solution(s) {
-    let splitWord = s.split(' ');
-    let changeWord = [];
-    let result = [];
+// 1. s를 공백을 기준으로 배열로 변경
+// 2. splitWord의 각 요소의 짝수 index는 대문자로, 홀수 index는 짝수로 변경하여 str에 담는다.
+// 3. 1개의 단어가 담긴 str을 changeWord에 담는다. (2-3의 과정을 마지막 단어까지 반복)
+// 4. 쉼표로 구분되어 있는 changeWord의 각 요소를 하나의 문자열로 만든 후 result에 담는다.
+// 5. result를 공백을 구분자로 하여 하나의 문자열로 만든다.
+function solution(s) {  // "try hello world"
+    let splitWord = s.split(' ');  // ["try","hello","world"]
+    let changeWord = [];  // // [["T","r","Y"],["H","e","L","l","O"],["W","o","R","l","D"]]
+    let result = [];  // ["TrY","HeLlO","WoRlD"]
 
     for (let i = 0; i < splitWord.length; i++) {
         let str = [];
@@ -18,6 +23,6 @@ function solution(s) {
     for (let i = 0; i < changeWord.length; i++) {
         result.push(changeWord[i].join(''))
     }
-    
+
     return result.join(' ');
 }
